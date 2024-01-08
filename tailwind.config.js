e65a439f93aab4f258b8d7224a95e5effb6f52e6
Gustavo Consoni <73336790+Gustavo-Consoni@users.noreply.github.com>
2024-01-08 15:15:52 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
+      screens: {
+        '2xl': '1552px',
+      },
       fontFamily: {
         teko: ['Teko', 'sans-serif'],
         montserrat: ['Montserrat', 'sans-serif'],

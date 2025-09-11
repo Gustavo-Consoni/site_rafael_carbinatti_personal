4241573bb067c133dac8bcca7ee1ef00d71ed787
Gustavo Consoni <73336790+Gustavo-Consoni@users.noreply.github.com>
2025-09-11 18:55:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ document.addEventListener('alpine:init', () => {
                 preco_centavos: ', 00',
                 juros: 'Em até 1x sem juros',
                 vantagens: [
-                    'Preço promocional R$ 180,00 à vista',
                     '1 planilha completa',
                     'Planejamento semanal de treino',
                     'Treino individualizado',
@@ -28,7 +27,6 @@ document.addEventListener('alpine:init', () => {
                 preco_centavos: ', 00',
                 juros: 'Em até 3x sem juros',
                 vantagens: [
-                    'Preço promocional R$ 495,00 à vista',
                     '2 planilhas completas',
                     'Planejamento semanal de treino',
                     'Treino individualizado',
@@ -44,7 +42,6 @@ document.addEventListener('alpine:init', () => {
                 preco_centavos: ', 00',
                 juros: 'Em até 6x sem juros',
                 vantagens: [
-                    'Preço promocional R$ 900,00 à vista',
                     '4 planilhas completas',
                     'Planejamento semanal de treino',
                     'Treino individualizado',
@@ -60,7 +57,6 @@ document.addEventListener('alpine:init', () => {
                 preco_centavos: ', 00',
                 juros: 'Em até 12x sem juros',
                 vantagens: [
-                    'Preço promocional R$ 1620,00 à vista',
                     '8 planilhas completas',
                     'Planejamento semanal de treino',
                     'Treino individualizado',
@@ -233,5 +229,6 @@ document.addEventListener('alpine:init', () => {
 
     }))
 })
+
 
 
